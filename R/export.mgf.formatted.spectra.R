9ffcb6d7cb21e98577f0b51b8477f8c60286fa16
Corey Broeckling <cbroeckl@users.noreply.github.com>
2025-05-14 16:35:21 -0600

    create 'export.mgf.formatted.spectra' function
    created  new function to support mgf formatted files

---
+++ R/export.mgf.formatted.spectra.R
@@ -0,0 +1,95 @@
+#' export.mgf.formatted.spectra
+#'
+#' @param ramclustObj ramclustR object to annotate. 
+#' @param output.dir character: path to write MGF formatted spectra to a new directory called 'spectra' is created (if it does not exist), and within it a directory called 'mgf'.  all spectra files are written to the 'spectra/ms' directory within the specified directory, or the working directory, if not specified. 
+#' @details This function exports spectra in .mgf format, suitable for importing into GNPS, NIST MSsearch, etc.
+#' @return  nothing - files written to disk
+#' @concept RAMClustR
+#' @concept metabolomics
+#' @concept mass spectrometry
+#' @concept xcms
+#' @author Corey Broeckling
+#' @export 
+#' 
+#' 
+export.mgf.formatted.spectra <- function(
+    ramclustObj = NULL,
+    output.dir = NULL
+) {
+  
+  if(is.null(ramclustObj$mse.spectra) & is.null(ramclustObj$dda.spectra)){
+    stop("neither mse nor dda spectra have been assigned. Please use one of 'assign.xcms.dda.peaks' or 'assign.xcms.mse.peaks' first.", '\n')
+  }
+  
+  
+  if(is.null(output.dir)) {
+    output.dir <- getwd()
+  }
+  if(!dir.exists(output.dir)) {
+    dir.create(output.dir)
+  }
+  if (!dir.exists(paste0(output.dir, "/spectra"))) {
+    dir.create(paste0(output.dir, "/spectra"))
+  }
+  if (!dir.exists(paste0(output.dir, "/spectra/mgf"))) {
+    dir.create(paste0(output.dir, "/spectra/mgf"))
+  }
+  
+  if(!is.null(ramclustObj$dda.spectra)) {
+    spec <- ramclustObj$dda.spectra
+  }
+  if(!is.null(ramclustObj$mse.spectra)) {
+    if(any(ls() == 'spec')) {
+      spec <- c(spec, ramclustObj$mse.spectra)
+    } else {
+      spec <- ramclustObj$mse.spectra
+    }
+    
+  }
+  
+  
+  
+  for (i in 1:length(spec)) {
+    if(!is.null(spec[[i]])) {
+      if(length(spec[[i]]) == 0) next
+      for(j in 1:length(spec[[i]])) {
+        sp.name <- paste(
+          spec[[i]][[j]]$ramclustr.cmpd, 
+          spec[[i]][[j]]$xcms.name, 
+          spec[[i]][[j]]$adduct.type,
+          spec[[i]][[j]]$spectrum.type, sep = ".")
+        out <- paste("BEGIN IONS", '\n',
+                     paste0("TITLE=", sp.name), '\n',
+                     paste0("PEPMASS=", round(spec[[i]][[j]]$precursor.mz, 5)), '\n',
+                     paste0("CHARGE=", spec[[i]][[j]]$charge), '\n',
+                     paste0("MSLEVEL=2"), '\n', sep = "")
+        
+        # out <- paste(out, '\n', 
+        #              ">collision ", spec[[i]][[j]]$collision.energy, '\n',
+        #              sep = "")
+        for (k in 1:nrow(spec[[i]][[j]]$spectrum)) {
+          out <- paste(out, round(spec[[i]][[j]]$spectrum$mz[k], 5), " ", round(spec[[i]][[j]]$spectrum$int[k], 2), "\n",
+                       sep = "")
+        }
+        out <- paste0(out, "END IONS", '\n')
+        write(out, file = paste0(output.dir, "/spectra/mgf/", sp.name,
+                                 ".mgf"))
+        rm(out)
+      }
+    }
+  }
+}
+
+# 
+# BEGIN IONS
+# PEPMASS=413.26611887841
+# CHARGE=1+
+#   TITLE=MS/MS scan at 1.535 min with Intensity: 604.0
+# 
+# 189.48956 1.9
+# 283.62076 3.4
+# 301.22977 66.3
+# 311.08008 1.3
+# 399.99106 2.3
+# 
+# END IONS
