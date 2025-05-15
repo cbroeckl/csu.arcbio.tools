41c10c853048173cdb0eb2a944b7e1994730d8d9
Corey Broeckling <cbroeckl@users.noreply.github.com>
2025-05-15 16:23:00 -0600

    add one.file option for mgf export

--- R/export.mgf.formatted.spectra.R
+++ R/export.mgf.formatted.spectra.R
@@ -2,6 +2,8 @@
 #'
 #' @param ramclustObj ramclustR object to annotate. 
 #' @param output.dir character: path to write MGF formatted spectra to a new directory called 'spectra' is created (if it does not exist), and within it a directory called 'mgf'.  all spectra files are written to the 'spectra/ms' directory within the specified directory, or the working directory, if not specified. 
+#' @param out.file logical: if TRUE, all spectra are exported in a single mgf file.  Else each spectrum is in its own file. default = TRUE
+#' @param out.file.name character: file name of mgf file when 'one.file' = TRUE. default = 'spectra.mgf'
 #' @details This function exports spectra in .mgf format, suitable for importing into GNPS, NIST MSsearch, etc.
 #' @return  nothing - files written to disk
 #' @concept RAMClustR
@@ -14,7 +16,9 @@
 #' 
 export.mgf.formatted.spectra <- function(
     ramclustObj = NULL,
-    output.dir = NULL
+    output.dir = NULL, 
+    one.file = TRUE,
+    one.file.name = "spectra.mgf"
 ) {
   
   if(is.null(ramclustObj$mse.spectra) & is.null(ramclustObj$dda.spectra)){
@@ -47,7 +51,9 @@ export.mgf.formatted.spectra <- function(
     
   }
   
-  
+  if(one.file) {
+    out.list <- as.list(rep(NA, 0))
+  }
   
   for (i in 1:length(spec)) {
     if(!is.null(spec[[i]])) {
@@ -72,24 +78,21 @@ export.mgf.formatted.spectra <- function(
                        sep = "")
         }
         out <- paste0(out, "END IONS", '\n')
-        write(out, file = paste0(output.dir, "/spectra/mgf/", sp.name,
+        if(one.file) {
+          out.list[[length(out.list)+1]] <- out
+        } else {
+          write(out, file = paste0(output.dir, "/spectra/mgf/", sp.name,
                                  ".mgf"))
+          }
+        
         rm(out)
       }
     }
   }
+  if(one.file) {
+    out <- unlist(out.list)
+    out <- paste(out, collapse = '\n')
+    write(out, file = paste0(output.dir, "/spectra/mgf/spectra.mgf"))
+  }
+  
 }
-
-# 
-# BEGIN IONS
-# PEPMASS=413.26611887841
-# CHARGE=1+
-#   TITLE=MS/MS scan at 1.535 min with Intensity: 604.0
-# 
-# 189.48956 1.9
-# 283.62076 3.4
-# 301.22977 66.3
-# 311.08008 1.3
-# 399.99106 2.3
-# 
-# END IONS
