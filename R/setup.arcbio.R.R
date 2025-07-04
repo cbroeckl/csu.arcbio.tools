303f1c7d9c657cc8d11db6a1857a9dd3b1b10b76
Corey Broeckling <cbroeckl@users.noreply.github.com>
2025-07-04 09:31:08 -0600

    Update setup.arcbio.R.R

--- R/setup.arcbio.R.R
+++ R/setup.arcbio.R.R
@@ -1,7 +1,7 @@
 #' setup.arcbio.R
 #'
 #' just a convenience function to set up all packages needed for routine metabolomics work at CSU ARC-BIO
-#' @details can use "source('https://github.com/cbroeckl/csu.arcbio.tools/tree/main/R/setup.arcbio.R'); setup.arcbio.R()" to run
+#' @details can use "source('https://github.com/cbroeckl/csu.arcbio.tools/tree/main/R/setup.arcbio.R.R'); setup.arcbio.R()" to run
 #' @return nothing.  all data are saved to disk for later loading
 #' @author Corey Broeckling
 #' 
@@ -48,43 +48,5 @@ setup.arcbio.R <- function() {
   devtools::install_github('cbroeckl/pubchem.bio', dependencies = TRUE, upgrade = 'always')
   
 }
-
-options(install.packages.check.source = "no")
-options(install.packages.compile.from.source = "no")
-
-installed <- installed.packages()[,c(1)]
-
-
-cran.packages <- c(
-  'devtools', 'BiocManager', 'rstudioapi', 'ggplot2', 'ggfortify', 'curl',
-  'emmeans', 'effects', 'lme4', 'lmerTest', 'pbkrtest', 'ClassDiscovery', 
-  'PCDimension', 'CHNOSZ', 'remotes'  
-)
-cran.packages <- cran.packages[!(cran.packages %in% installed)]
-
-if(length(cran.packages) > 0) {
-  for(i in 1:length(cran.packages)) {
-    install.packages(cran.packages[i], dependencies = TRUE, update = TRUE, ask = FALSE, repos='https://mirror.las.iastate.edu/CRAN/', verbose = FALSE)
-  }
-}
-
-bioc.packages <- c(
-  'biobase', 'biocgenerics',
-  'MsCoreUtils', 'MetaboCoreUtils', 'Spectra', 'MetaboAnnotation', 'CompoundDb',
-  'MsBackendMgf', 'MsBackendMassbank', 'MsBackendMsp', 'mzR', 'xcms', 'ChemmineR', 'ChemmineOB'
-)
-
-bioc.packages <- bioc.packages[!(bioc.packages %in% installed)]
-if(length(bioc.packages) > 0) {
-  for(i in 1:length(bioc.packages)) {
-    BiocManager::install(bioc.packages[i], dependencies = TRUE, update = TRUE, ask = FALSE, verbose = FALSE)
-  }
-}
-
-## from GITHUB
-devtools::install_github('cbroeckl/RAMClustR', dependencies = TRUE, upgrade = 'always')
-devtools::install_github('cbroeckl/csu.pmf.tools', dependencies = TRUE, upgrade = 'always')
-devtools::install_github('cbroeckl/csu.arcbio.tools', dependencies = TRUE, upgrade = 'always')
-devtools::install_github('cbroeckl/pubchem.bio', dependencies = TRUE, upgrade = 'always')
 
 
