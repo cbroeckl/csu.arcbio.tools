d67cb333a35b1fc8c4f4ee2783d3f62daafb8081
Corey Broeckling <4959997+cbroeckl@users.noreply.github.com>
2025-09-11 14:03:15 -0600

    update fiora.mgf.to.specta
    and documentation

--- R/fiora.mgf.to.spectra.R
+++ R/fiora.mgf.to.spectra.R
@@ -5,6 +5,7 @@
 #' @param mgf.dir character file path.  directory containing fiora output .mgf files.  
 #' @param split.by.polarity logical.  if true, spectra are split into positive ('1') and negative ('0') polarities before saving. default = TRUE.
 #' @param out.dir character. full file path to save 'Spectra' object(s) to.  
+#' @param return.library logical.  If true, return library to R console.  
 #' @return spectra object.  File(s) also sent to output directory 'out.dir'.    
 #' @author Corey Broeckling
 #' 
@@ -14,15 +15,18 @@
 fiora.mgf.to.spectra <- function(
     mgf.dir = NULL,
     split.by.polarity = TRUE,
-    out.dir = NULL
-    ) {
-
+    out.dir = NULL, 
+    return.library = TRUE
+) {
+  
   requireNamespace('Spectra')
   requireNamespace('MsBackendMgf')
   
   
   if(!dir.exists(mgf.dir)) stop("mgf.dir does not exist: ", mgf.dir, '\n')
   mgf.files <- list.files(mgf.dir, pattern = "mgf", full.names = TRUE)
+  # mgf.files <- mgf.files[1:1000]
+  
   
   if(is.null(out.dir)) {
     out.dir <- mgf.dir
@@ -34,9 +38,29 @@ fiora.mgf.to.spectra <- function(
   
   normalize.intensity <- function(x, ...) {
     maxint <- max(x[, 2], na.rm = TRUE)
-    x[, 2] <- as.integer(100 * x[, 2] / maxint)
+    x[, 2] <- as.integer(1000 * x[, 2] / maxint)
     x
   }
+  
+  # ## read in all individual .mgf files and export to single file called 'all.mgf'
+  # 
+  # if(aggregate.files) {
+  #   tmp <- as.list(rep(NA, length(mgf.files)))
+  #   for(i in 1:length(mgf.files)) {
+  #     tmp[[i]] <- paste(readLines(mgf.files[i]), collapse = '\n')
+  #     # tmp <- c(tmp, tm)
+  #     # cat(i, " ")
+  #   }
+  #   
+  #   out <- sapply(tmp, paste, collapse= '\n')
+  #   sink(paste0(mgf.dir, "/all.MGF"))
+  #   cat(out)
+  #   sink()
+  #   rm(out)
+  #   rm(tmp)
+  #   gc()
+  # }
+  # 
   
   mapping = c(
     title = "TITLE",
@@ -49,51 +73,42 @@ fiora.mgf.to.spectra <- function(
     comment = "COMMENT"
   )
   
-  for(i in 1:length(mgf.files)) {
-    cat(i, " ")
-    ## read in mgf file
-    
-    suppressMessages(s <- backendInitialize(MsBackendMgf::MsBackendAnnotatedMgf(), mgf.files[i], mapping = mapping))
-    add.type <- s$adduct
-    polarity <- rep(1, length(add.type))
-    is.neg <- grep("[M-H]-", add.type, fixed = TRUE)
-    if(any(is.neg)){
-      polarity[is.neg] <- 0
-    }
-    polarity(s) <- polarity
-    plotSpectra(s[1])
-    nm <- s$title
-    cid <- as.numeric(sapply(1:length(nm), FUN = function(x) unlist(strsplit(nm[x], "_"))[1]))
-    s$cid <- as.integer(cid)
-    s$precursorCharge <- 1L
-    s <- Spectra::Spectra(s)
-    
-    ## clean spectra
-    s <- filterMzRange(s, c(50,2000))
-    s <- addProcessing(s, normalize.intensity)
-    
-    ## apply processing
-    s <- applyProcessing(s)
-    
-    ## bind spectra to prior spectra
-    if(i == 1) {
-      fiora.library <- s
-    } else {
-      fiora.library <- concatenateSpectra(fiora.library, s)
-    }
-    rm(s)
-    gc()
-    
+  # if(aggregate.files) {
+  #   fiora.library <- Spectra::backendInitialize(MsBackendMgf::MsBackendAnnotatedMgf(), paste0(mgf.dir, "/all.MGF"), mapping = mapping)
+  # } else {
+    fiora.library <- Spectra::backendInitialize(MsBackendMgf::MsBackendAnnotatedMgf(), mgf.files, mapping = mapping)
+  # }
+  # print(mgf.files)
+  add.type <- fiora.library$adduct
+  polarity <- rep(1L, length(add.type))
+  is.neg <- grep("[M-H]-", add.type, fixed = TRUE)
+  if(any(is.neg)){
+    polarity[is.neg] <- 0L
   }
+  fiora.library$polarity <- polarity
+  nm <- fiora.library$title
+  cid <- as.numeric(sapply(1:length(nm), FUN = function(x) unlist(strsplit(nm[x], "_"))[1]))
+  fiora.library$cid <- as.integer(cid)
+  fiora.library$precursorCharge <- 1L
+  fiora.library <- Spectra::Spectra(fiora.library)
   
-  fiora.library <- Spectra::setBackend(fiora.library, backend = MsBackendMemory())
+  ## clean spectra
+  fiora.library <- Spectra::filterMzRange(fiora.library, c(50,2000))
+  fiora.library <- Spectra::addProcessing(fiora.library, normalize.intensity)
+  
+  ## apply processing
+  fiora.library <- Spectra::applyProcessing(fiora.library)
+  
+  gc()
+  
+  fiora.library <- Spectra::setBackend(fiora.library, backend = Spectra::MsBackendMemory())
   save(fiora.library, file = paste0(mgf.dir, "fiora.pos.Rdata"))
   
   if(split.by.polarity) {
-    fiora.pos <- Spectra::filterPolarity(fiora.library, 1)
-    fiora.neg <- Spectra::filterPolarity(fiora.library, 0)
+    fiora.pos <- Spectra::filterPolarity(fiora.library, 1L)
+    fiora.neg <- Spectra::filterPolarity(fiora.library, 0L)
     if(length(fiora.pos) > 0) save(fiora.pos, file = paste0(mgf.dir, "fiora.pos.Rdata"))
-    if(length(fiora.neg) > 0) save(fiora.neg, file = paste0(mgf.dir, "fiora.pos.Rdata"))
+    if(length(fiora.neg) > 0) save(fiora.neg, file = paste0(mgf.dir, "fiora.neg.Rdata"))
     rm(fiora.library)
     fiora.library <- as.list(rep(NA, 2))
     names(fiora.library) <- c("pos", "neg")
@@ -101,6 +116,8 @@ fiora.mgf.to.spectra <- function(
     fiora.library[[2]] <- fiora.neg
   }
   
-  return(fiora.library)
+  if(return.library) {
+    return(fiora.library)
+  }
   
 }
